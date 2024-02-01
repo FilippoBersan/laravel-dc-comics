26aa28f9581b16379b5bf8f096d2b77568c1d906
FilippoBersan <filippobersan99@gmail.com>
2024-02-01 17:51:47 +0100

    feat: seeder

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,21 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        
+        $comics = config('comic_db');
+        
+        foreach($comics as $comic_item){
+            $comic = new Comic();
+            $comic->title = $comic_item['title'];
+             $comic->thumb = $comic_item['thumb'];
+            $comic->description = $comic_item['description'];
+            $comic->price = $comic_item['price'];
+             $comic->series = $comic_item['series'];
+              $comic->sale_date = $comic_item['sale_date'];
+            $comic->type = $comic_item['type'];
+            //  $comic->artists = $comic_item['artists']; // BONUS array to string
+            //   $comic->writers = $comic_item['writers'];
+            $comic->save();
+        }
     }
 }
